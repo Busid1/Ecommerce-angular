fcbb6504b07ad3fed6bfc3fa9dd05e952eadd520
Busid <desarrolloweb214@gmail.com>
2024-11-08 16:46:44 +0100

    first-commit

---
+++ src/app/products/features/product-detail/product-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, effect, inject, input } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import ProductDetailStateService from '../../data-access/product-detail-state.service';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './product-detail.component.html',
+  providers: [ProductDetailStateService]
+})
+export default class ProductDetailComponent {
+  productDetailState = inject(ProductDetailStateService).state;
+  id = input.required<string>();
+
+  constructor(){
+    effect(()=> {
+      this.productDetailState.getById(this.id());
+    })
+  }
+}
